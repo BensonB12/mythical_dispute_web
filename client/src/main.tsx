00f3db790e447479b29f867de38ba8bdcb2373ab
BensonB12 <bensonbird2@gmail.com>
2024-11-15 00:45:32 -0700

    sass-stinks

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { AppAuthProvider } from "./features/auth/AppAuthProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
-import "./../custom.scss";
+// import "./../custom.scss";
 import "bootstrap";
 import { getQueryClient } from "./utils/queryClient.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
