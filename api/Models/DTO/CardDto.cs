da86b7300405554617a3897161347d174ad1ae51
BensonB12 <bensonbird2@gmail.com>
2024-11-20 22:04:30 -0700

    format

--- api/Models/DTO/CardDto.cs
+++ api/Models/DTO/CardDto.cs
@@ -5,25 +5,25 @@ namespace api.Models;
 
 public partial class CardDTO
 {
-  public int Id { get; set; }
+    public int Id { get; set; }
 
-  public string CardName { get; set; } = null!;
+    public string CardName { get; set; } = null!;
 
-  public string AnimalClass { get; set; } = null!;
+    public string AnimalClass { get; set; } = null!;
 
-  public string Family { get; set; } = null!;
+    public string Family { get; set; } = null!;
 
-  public string Size { get; set; } = null!;
+    public string Size { get; set; } = null!;
 
-  public string? AirValue { get; set; }
+    public string? AirValue { get; set; }
 
-  public int? LandValue { get; set; }
+    public int? LandValue { get; set; }
 
-  public int? WaterValue { get; set; }
+    public int? WaterValue { get; set; }
 
-  public string? TextBox { get; set; }
+    public string? TextBox { get; set; }
 
-  public string? ImgUrl { get; set; }
+    public string? ImgUrl { get; set; }
 
-  public string? ArtistName { get; set; }
+    public string? ArtistName { get; set; }
 }
