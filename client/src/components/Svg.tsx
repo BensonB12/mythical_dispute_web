331c50170a4d562fd1010e417a61af5361286df3
BensonB12 <bensonbird2@gmail.com>
2024-12-04 10:01:45 -0700

    NoReplace

--- client/src/components/Svg.tsx
+++ client/src/components/Svg.tsx
@@ -1,16 +1,5 @@
 import { FC } from "react";
 
 export const Svg: FC<{ src: string }> = ({ src }) => {
-  return (
-    <img
-      src={src}
-      alt={`A picture of ${src
-        .split("/")
-        .pop()!
-        .split(".")
-        .slice(-2, -1)[0]
-        .replace(/[_-]/g, " ")}`}
-      height={60}
-    />
-  );
+  return <img src={src} alt={`A picture of ${src}`} height={60} />;
 };
